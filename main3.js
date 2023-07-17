cab1de68ceece4306951f3600fd294ab0ebfa6dd
nouris24-meet <nour.stety@gmail.com>
2023-07-17 14:18:05 +0300

    I DID THE BONUSSSSSS!!!!

--- main3.js
+++ main3.js
@@ -1,3 +1,12 @@
 function changeBackgroundColor(color) {
 	document.body.style.background= color; 
 }
+function changeBandF(color1,color2) {
+	var n = document.body.style.background; 
+	if (n==color1){
+		document.body.style.background= color2; 
+	}
+	else{
+		document.body.style.background= color1;
+	}
+}
